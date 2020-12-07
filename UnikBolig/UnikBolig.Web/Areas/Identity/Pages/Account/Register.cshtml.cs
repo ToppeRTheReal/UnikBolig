9127b00429cc30474e79f833fd7110295c36ea57
Thomas Clausen <topperhdoriginal@gmail.com>
2020-12-07 23:01:50 +0100

    filter function and waiting list functionality added

--- UnikBolig/UnikBolig.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ UnikBolig/UnikBolig.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Unik.Areas.Identity.Data;
+using UnikBolig.Application;
 
 namespace Unik.Areas.Identity.Pages.Account
 {
@@ -89,45 +90,18 @@ namespace Unik.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-            if (ModelState.IsValid)
+            IUserHandler handler = new UserHandler(null);
+            try
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName};
-                var result = await _userManager.CreateAsync(user, Input.Password);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
+                handler.Create(Guid.NewGuid(), Input.FirstName, Input.LastName, Input.Email, Input.Phone, Input.Password);
+                return RedirectToRoute("/Identity/Account/Login");
             }
-
-            // If we got this far, something failed, redisplay form
-            return Page();
+            catch
+            {
+                // handler error view here
+                return Page();
+            }
+            
         }
     }
 }
