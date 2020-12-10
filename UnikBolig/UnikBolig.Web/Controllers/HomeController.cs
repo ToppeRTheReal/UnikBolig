967d332bdb0ab069e49fbd0bb800d09223ad595e
User0183275 <PapaCode@users.noreply.github.com>
2020-12-10 09:56:18 +0100

    Udlej bolig / login
    Kan ikke helt teste login før programmet kører igen

--- UnikBolig/UnikBolig.Web/Controllers/HomeController.cs
+++ UnikBolig/UnikBolig.Web/Controllers/HomeController.cs
@@ -39,6 +39,11 @@ namespace Unik.Controllers
             return View();
         }
 
+        public IActionResult Udlej()
+        {
+            return View();
+        }
+
        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
